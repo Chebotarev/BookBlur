7900e11101a20a9b0e3dc9bcdf7429b3bd5d8908
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-06-09 14:36:57 -0700

    Add books model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
   root "sessions#new"
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609180942) do
+ActiveRecord::Schema.define(version: 20150609213253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "author"
+    t.string   "url",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "books", ["author"], name: "index_books_on_author", using: :btree
+  add_index "books", ["title", "author", "url"], name: "index_books_on_title_and_author_and_url", unique: true, using: :btree
+  add_index "books", ["title"], name: "index_books_on_title", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
