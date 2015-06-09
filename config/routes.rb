1979bf505e321c576990a2b8ded39b2d46c1e025
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-06-09 12:41:11 -0700

    Implement Sign In page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "sessions#new" 
+  root "sessions#new"
   resources :users, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 end
