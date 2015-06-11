da698c341fc506ded2760b833ab7ee76c292e306
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-06-10 17:32:01 -0700

    Basic ListNew elements with book search bar

--- app/assets/javascripts/views/lists/new.js
+++ app/assets/javascripts/views/lists/new.js
@@ -1,9 +1,16 @@
 BookBlur.Views.ListNew = Backbone.View.extend({
   template: JST['lists/new'],
 
+  className: "container",
+
   render: function () {
     var content = this.template();
     this.$el.html(content);
+    this.onRender();
     return this;
+  },
+
+  onRender: function () {
+    $(".search-container").bookSearch();
   }
 });
